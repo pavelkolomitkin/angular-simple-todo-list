516cc7c70408132ed8bb0f8501583912bdb17ed4
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2018-09-08 02:25:51 +0300

    add editing of the title of category

--- src/app/category-list/category-details/category-details.component.ts
+++ src/app/category-list/category-details/category-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AfterContentChecked, AfterViewChecked, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {Category} from '../../models/category.model';
 import {TodoList} from '../../models/todolist.model';
 
@@ -7,15 +7,21 @@ import {TodoList} from '../../models/todolist.model';
   templateUrl: './category-details.component.html',
   styleUrls: ['./category-details.component.css']
 })
-export class CategoryDetailsComponent implements OnInit {
+export class CategoryDetailsComponent implements OnInit, AfterViewChecked {
 
   @Input() category: Category = null;
-
   @Output('onDelete') onDeleteCategory: EventEmitter<Category> = new EventEmitter<Category>();
 
-  constructor() { }
+  @ViewChild('editableTitleInput') editableTitleElement: ElementRef;
+
+  isTitleEditing: boolean = false;
+
+  editableTitle: string = '';
+
+  constructor() {}
 
   ngOnInit() {
+
   }
 
   onTodoListCreateHandler(todoList: TodoList)
@@ -27,5 +33,44 @@ export class CategoryDetailsComponent implements OnInit {
   {
       this.onDeleteCategory.emit(category);
   }
+
+  onEditTitleStartHandler($event)
+  {
+      this.editableTitle = this.category.title;
+
+      this.isTitleEditing = true;
+  }
+
+  onEditTitleEnterKeyUp(value: string)
+  {
+      this.processEditingTitle(value);
+  }
+
+  onEditTitleBlurHandler(value: string)
+  {
+      this.processEditingTitle(value);
+  }
+
+  private processEditingTitle(value)
+  {
+      if (value.trim() !== '' && this.isTitleEditing)
+      {
+          this.category.title = value.trim();
+          this.isTitleEditing = false;
+      }
+  }
+
+  onEditTitleEscapeHandler()
+  {
+      this.isTitleEditing = false;
+  }
+
+
+    ngAfterViewChecked(): void {
+        if (this.editableTitleElement)
+        {
+            this.editableTitleElement.nativeElement.focus();
+        }
+    }
 
 }
